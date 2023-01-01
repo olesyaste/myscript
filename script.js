33e40c3c58575fe1c85f450d33c0b3ba5e5ed90f
Olesya Steinmetz <newsabome@icloud.com>
2023-01-01 19:57:30 +0100

    Modified my script

--- script.js
+++ script.js
@@ -1,50 +1,69 @@
-const puppeteer = require('puppeteer');
-const urls = [];
-(async () => {
-const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized'], userDataDir: '<PATH>'});
+const puppeteer = require('puppeteer-core')
+const { executablePath } =  require('puppeteer');
+const chromium =  require('chrome-aws-lambda')
 
+const urls = []
+async function click() {
+const browser = await puppeteer.launch({ headless: false, executablePath: executablePath(),  args: [ ...chromium.args, '--start-maximized', '--no-sandbox'], userDataDir: '<PATH>'});
 const page = await browser.newPage();
+await page.setViewport({
+  width: 840,
+  height: 720,
+  deviceScaleFactor: 1,
+});
 let pages
 let prices = []
-let newRelevantPages
-let newPages
-    for (let i = 0; i <= urls.length; i++) {
+let newRelevantPages = []
+let newpage
+let newPages = []
+    for (let i = 0; i < urls.length; i++) {
         const url = urls[i];
         console.log(urls[i])
         await page.goto(`${url}`, { waitUntil: 'networkidle2' });
-        await page.type('#whereSayt', 'Dubai');
-        await page.$eval('#arrival', el => el.value = '01/22/2023');
-        await page.$eval('#departure', el => el.value = '01/26/2023');
-        await page.click('#booking-submit');
-        pages = await browser.pages();
+        await page.click('button[data-osc-product="search-form-dates"]')
+        await page.waitForTimeout(2000)
+        await page.evaluate(() => {
+          /*var myNodeList = document.querySelectorAll('h2[data-testid="calendar-month-header"]');
+          Array.from(myNodeList).filter(function(el) {
+          //console.log(el.innerText)
+            if (el.innerText != 'March 2023'){*/
+          Array.from(document.querySelectorAll('span')).filter(span => {
+            return span.innerText == 'Next Month'
+          }).forEach(element => {
+            if (element) element.click();
+          });
+       // };
+        })
+        page.waitForNavigation()
+        await page.waitForTimeout(4000)
+        await page.click('button[id="day-1700262000000"]');
+        await page.click('button[id="day-1700953200000"]');
+        await page.click('path[class="stroke-text"]');
+        await page.type('input[class="form-input w-full"]', 'Miami Beach');
+        page.waitForNavigation()
         await page.waitForTimeout(3000)
-        newRelevantPages = await pages.splice(2)
-        console.log(newRelevantPages)
-        for (newPage of newRelevantPages){
-           await newPage.bringToFront({waitUntil: 'networkidle2'})
-        }
-        await pages[1].bringToFront({waitUntil: 'networkidle2'})
-
-    }
-    for (p of newRelevantPages) {
-              prices.push(await p.$$eval('[data-testid="hotel-card-content"]', (els) => els.map((el) => {
-                 return {
-                    company: el.querySelector('[class="text-xs text-text-alt"]').innerText,
-                    hotel: el.querySelector('[data-testid="listViewPropertyName"]').innerText,
-                    price: el.querySelector('[class="text-primary font-bold numeric-tabular-nums leading-none rtl:text-left text-lg md:text-xl"]').innerText.replace(/\D/g, "")
-                         }
-              })))
+        await page.click('button[data-osc-product="search-form-button"]')
+        page.waitForNavigation()
+        await page.waitForTimeout(3000)
+        await page.click('button[data-osc-product="search-form-button"]')
+        await page.waitForNavigation()
+        await page.waitForTimeout(5000)
+        newpage = await browser.pages();
+        newRelevantPages = newpage[1]
+                prices.push(await newRelevantPages.$$eval('[data-testid="hotel-card-content"]', (els) => els.map((el) => {
+                   return {
+                      company: el.querySelector('[class="text-xs text-text-alt"]').innerText,
+                      hotel: el.querySelector('h3[data-testid="listViewPropertyName"]').innerText,
+                      price: el.querySelector('[data-testid="priceInfo"]').innerText.replace(/\D/g, "")
+                   }
+                })))
     }
   let data = prices.flat(1)
   let hotels = []
-  console.log(data)
-  Promise.all(newRelevantPages).then(() => {
-        browser.close();
-  })
     const XLSX = require('xlsx')
     let binaryWS = XLSX.utils.json_to_sheet(data);
     var wb = XLSX.utils.book_new()
     XLSX.utils.book_append_sheet(wb, binaryWS, 'Hotels')
     XLSX.writeFile(wb, 'hotels.xlsx');
-})
-()
+}
+click()
